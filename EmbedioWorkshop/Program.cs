e899131565998c4ab7c3975227a6c822d87a6e6a
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-08-18 14:41:31 -0500

    Initial program

---
+++ EmbedioWorkshop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using EmbedIO;
+
+namespace EmbedioWorkshop
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var server = new WebServer("http://localhost:1234");
+
+            server.RunAsync();
+            
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
